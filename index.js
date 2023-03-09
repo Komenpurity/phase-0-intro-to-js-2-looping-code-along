2148fd68a067376e9c6553f012ca253945c96536
Komenpurity <jepkorirpurity8@gmail.com>
2023-03-09 20:08:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const sinon = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(sinon){
+    for (let i = 0; i < sinon.length; i++) { 
+    console.log(`Thank you, ${sinon[i]}, for the wonderful surprise gift!`); 
+    debugger;
+    }
+    return sinon;
+}
+writeCards();
+
+
+function countDown(number) {
+    let posNumber = number;
+    while (posNumber > -1) {
+         console.log(posNumber);
+         posNumber--;
+    }
+}
